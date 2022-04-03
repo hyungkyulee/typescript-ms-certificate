c87f99e486519776b0376bbfc5e6f48a6b6c3d23
Kyu Lee <Kyu.Lee@sainsburys.co.uk>
2022-04-03 22:30:35 +0100

    closure test

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,32 @@
 "use strict";
-/* EXERCISE 4
-   TODO: Declare the array as the type to match the type of the items in the array. */
-let randomNumbers = [];
-let nextNumber;
-for (let i = 0; i < 10; i++) {
-    nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-    randomNumbers.push(nextNumber);
+// /* EXERCISE 4
+//    TODO: Declare the array as the type to match the type of the items in the array. */
+//    let randomNumbers: Array<number> = [];
+//    let nextNumber;
+//    for (let i = 0; i < 10; i++) {
+//        nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+//        randomNumbers.push(nextNumber);
+//    }
+//    console.log(randomNumbers);
+// function addClick(items: any) { 
+//     for (var i = 0; i < items.length; i++) { 
+//         items[i].onClick = function () { 
+//             return i; 
+//         }; 
+//     } 
+//     return items; 
+// } 
+function addClick(items) {
+    for (var i = 0; i < items.length; i++) {
+        items[i].onClick = (function (i) {
+            return function () {
+                return i;
+            };
+        }(i));
+    }
+    return items;
 }
-console.log(randomNumbers);
+const example = [{}, {}];
+const clickSet = addClick(example);
+console.log(`result: ${clickSet[0].onClick()}, ${clickSet[1].onClick()}`);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,35 @@
-/* EXERCISE 4
-   TODO: Declare the array as the type to match the type of the items in the array. */
+// /* EXERCISE 4
+//    TODO: Declare the array as the type to match the type of the items in the array. */
 
-   let randomNumbers: Array<number> = [];
-   let nextNumber;
-   for (let i = 0; i < 10; i++) {
-       nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
-       randomNumbers.push(nextNumber);
-   }
+//    let randomNumbers: Array<number> = [];
+//    let nextNumber;
+//    for (let i = 0; i < 10; i++) {
+//        nextNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+//        randomNumbers.push(nextNumber);
+//    }
    
-   console.log(randomNumbers);
+//    console.log(randomNumbers);
+
+// function addClick(items: any) { 
+//     for (var i = 0; i < items.length; i++) { 
+//         items[i].onClick = function () { 
+//             return i; 
+//         }; 
+//     } 
+//     return items; 
+// } 
+
+function addClick(items: any[]) {
+    for (var i = 0; i < items.length; i++) {
+        items[i].onClick = (function (i) { 
+            return function () {
+                return i; 
+            };
+        }(i));
+    }
+    return items;
+}
+
+const example = [{}, {}]
+const clickSet = addClick(example)
+console.log(`result: ${clickSet[0].onClick()}, ${clickSet[1].onClick()}`)
